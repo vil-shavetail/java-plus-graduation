60163d97b675026dbae40e4de51ec19423ec0885
vil-shavetail <demid.ivan@gmail.com>
2026-03-16 16:07:14 +0300

    Sprint 24 - Task Three, added template for the aggregator service.

--- stats/aggregator/src/main/java/ru/practicum/AggregatorApplication.java
+++ stats/aggregator/src/main/java/ru/practicum/AggregatorApplication.java
@@ -1,7 +1,13 @@
 package ru.practicum;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
 public class AggregatorApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(AggregatorApplication.class, args);
     }
 }
