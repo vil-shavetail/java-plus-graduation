19502dd66b3c64a1452ae4de662a6069a2725460
vil-shavetail <demid.ivan@gmail.com>
2026-03-20 14:27:53 +0300

    Sprint 24 - Task Three, made final fixes.

--- core/event-service/src/main/java/ru/practicum/controller/event/PublicEventController.java
+++ core/event-service/src/main/java/ru/practicum/controller/event/PublicEventController.java
@@ -7,7 +7,6 @@ import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.AnalyzerClient;
